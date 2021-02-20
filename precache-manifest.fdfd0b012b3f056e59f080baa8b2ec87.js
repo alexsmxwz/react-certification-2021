9530f3a4a34961a2b683580553a35bb8a95ce709
Alejandro Santamaria wz <alejandro.santamaria@wizeline.com>
2021-02-19 20:43:59 -0600

    Updates

---
+++ precache-manifest.fdfd0b012b3f056e59f080baa8b2ec87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bff39a50388eeacefe76716aa3a1d613",
+    "url": "/react-certification-2021/index.html"
+  },
+  {
+    "revision": "2b5d6f6650474a6df3d5",
+    "url": "/react-certification-2021/static/js/2.22829fa1.chunk.js"
+  },
+  {
+    "revision": "d2266803016540034565984c8bb59069",
+    "url": "/react-certification-2021/static/js/2.22829fa1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4fc14437941e78afd59",
+    "url": "/react-certification-2021/static/js/main.9e48d2da.chunk.js"
+  },
+  {
+    "revision": "c25ef8b25a78f0d18d30",
+    "url": "/react-certification-2021/static/js/runtime-main.d69dc15b.js"
+  }
+]);
